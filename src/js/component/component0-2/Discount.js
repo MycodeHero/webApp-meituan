90005ea16761dce349b893e94f2f60cff83bbbbf
MyCode <soloboy_ok@163.com>
2017-03-21 17:45:50 +0800

    test

---
+++ src/js/component/component0-2/Discount.js
@@ -0,0 +1,77 @@
+require('../../../less/less0-2/Discount.less')
+//引入react的模块依赖
+var React = require('react');
+
+//标题组件
+var Title = React.createClass({
+    getDefaultProps: function(){
+        return {
+            style: {
+                width:'100%',
+                height:'33px',
+                lineHeight:'33px',
+                fontWeight:550,
+            }
+        }
+    },
+    render: function(){
+         return (
+            <p style={{textAlign:'center',paddingTop:'9px'}}>—- <span style={this.props.style}>优惠专区</span> -—</p>
+        )
+    }
+})
+
+//优惠种类组件
+var DiscountItem = React.createClass({
+    componentWillMount: function(){
+        var kind = this.props.kindItem;
+        var rows = [];
+        kind.forEach(function(ele, index){
+            rows.push(<li key={index} style={{background:'url(' + ele.imgSrc + ') 0% 0%/ 100% 100%'}}>
+                <div className={'dis-text'}>
+                    <h4>{ele.text}</h4>
+                    <p>{ele.discount}</p>
+                </div>
+            </li>)
+        })
+        this.rows = rows;
+    },
+    render: function(){
+        return (
+            <div style={{display:'flex'}}>
+                <div className={'dis-left'}>
+                    <ul>
+                        {
+                            this.rows.slice(0, 1)
+                        }
+                    </ul>
+                </div>
+                <div className={'dis-right'}>
+                    <ul className={'layout'}>
+                        {
+                            this.rows.slice(1, 3)
+                        }
+                    </ul>
+                    <ul className={'layout'}>
+                        {
+                            this.rows.slice(3, 5)
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+})
+
+//优惠组件
+var Discount = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <Title/>
+                <DiscountItem kindItem = {this.props.data}/>
+            </div>
+        )
+    }
+})
+module.exports = Discount;
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,14 +1,17 @@
+require('../less/init.less');
+require('../less/index.less');
+var Discount = require('./component/component0-2/Discount.js');
 var React = require('react');
 var ReactDom = require('react-dom');
+var data = [
+    {text: '用心早餐', discount: '不止八折', big: true, imgSrc:'./src/img/page0/page0-2/0.png'},
+    {text: '天天满减', discount: '优惠想不停', big: false, imgSrc:'./src/img/page0/page0-2/1.png'},
+    {text: '披萨专场', discount: '资本主义美食', big: false, imgSrc:'./src/img/page0/page0-2/2.png'},
+    {text: '领券中心', discount: '可叠加使用', spe:true, big: false, imgSrc:'./src/img/page0/page0-2/3.png'},
+    {text: '超级折扣菜', discount: '劲爆折扣', big: false, imgSrc:'./src/img/page0/page0-2/4.png'}
+]
 
-var App = React.createClass({
-    render: function () {
-        return (
-            <h1>We are Team</h1>
-        )
-    }
-});
 ReactDom.render(
-    <App/>,
-    document.getElementById('appWrap')
+    <Discount data={data}/>,
+    document.getElementsByClassName('prefecture')[0]
 )
