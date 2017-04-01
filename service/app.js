d4978d74d8dbafb67ea901e234e53991a9adf641
MyCode <soloboy_ok@163.com>
2017-04-01 09:48:45 +0800

    test

--- service/app.js
+++ service/app.js
@@ -13,6 +13,22 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+app.all('*', function(req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("X-Powered-By",' 3.2.1')  
+    res.header("Content-Type", "application/json;charset=utf-8");  
+    next();  
+});  
+  
+app.get('/auth/:id/:password', function(req, res) {  
+    res.send({id:req.params.id, name: req.params.password});  
+});  
+  
+app.listen(8080);  
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,5 +1,6 @@
-import $ from 'n-zepto'
+import $ from 'n-zepto';
+require('./tools/jqFP.js');
+require('./tools/jqUI.js');
 require('../less/init.less');
 var component01 = require('./component/component0-1/component0-1.js');
 var component02 = require('./component/component0-2/component0-2.js');
-
